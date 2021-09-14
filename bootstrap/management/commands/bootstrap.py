ce83614b6188eadf77b45bdbde6c5b4b08d2b4ae
Ryan Pelavin <rpelavin@gmail.com>
2021-09-14 17:21:59 -0400

    bootstrapping admin superuser

--- bootstrap/management/commands/bootstrap.py
+++ bootstrap/management/commands/bootstrap.py
@@ -1,7 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     help = 'Bootstraps the environment with default development data'
 
     def handle(self, *args, **options):
-        self.stdout.write(self.style.SUCCESS('TODO: boostrap here'))
+        User = get_user_model()
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(username='admin',
+                                          email='admin@local',
+                                          password='password')
+            self.stdout.write(self.style.SUCCESS('Created admin superuser.'))
