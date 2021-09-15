82293b9c7cd2c7277be9cedae7d7962fa02fff5a
Ryan Pelavin <rpelavin@gmail.com>
2021-09-15 10:43:50 -0400

    adding string representation to Tenant model

--- tenants/models.py
+++ tenants/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django_tenants.models import TenantMixin, DomainMixin
 
+
 class Tenant(TenantMixin):
     name = models.CharField(max_length=100)
     created_on = models.DateField(auto_now_add=True)
 
     # default true, schema will be automatically created and synced when it is saved
     auto_create_schema = True
+
+    def __str__(self):
+        return self.name
+
 
 class Domain(DomainMixin):
     pass
